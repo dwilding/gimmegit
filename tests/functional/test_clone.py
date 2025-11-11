a03ccabc8b8f58a60b3b31d16d6d5aa712bf0f82
Dave Wilding <tech@dpw.me>
2025-11-11 08:24:09 +0800

    add test and reorganize tests

--- tests/functional/test_clone.py
+++ tests/functional/test_clone.py
@@ -1,8 +1,5 @@
 from pathlib import Path
-import os
 import subprocess
-
-import pytest
 
 import helpers_functional as helpers
 
@@ -203,60 +200,3 @@ Cloned repo:
 {expected_dir}
 """
     assert result.stdout == expected_stdout
-
-
-@pytest.fixture
-def askpass_env():
-    env = os.environ.copy()
-    env["GIT_ASKPASS"] = "/bin/true"
-    return env
-
-
-def test_invalid_repo(uv_run, test_dir, askpass_env):
-    command = [
-        *uv_run,
-        "gimmegit",
-        *helpers.no_color,
-        *helpers.no_ssh,
-        "dwilding/invalid",
-        "my-feature",
-    ]
-    result = subprocess.run(
-        command,
-        cwd=test_dir,
-        env=askpass_env,
-        capture_output=True,
-        text=True,
-    )
-    assert result.returncode == 1
-    expected_stdout = """\
-Getting repo details
-Cloning https://github.com/dwilding/invalid.git
-"""
-    assert result.stdout == expected_stdout
-    expected_stderr = """\
-Error: Unable to clone repo. Is the repo private? Try configuring Git to use SSH.
-"""
-    assert result.stderr == expected_stderr
-    assert (Path(test_dir) / "invalid").exists()
-    assert not (Path(test_dir) / "invalid/dwilding-my-feature").exists()
-
-
-def test_no_repo(uv_run, test_dir):
-    command = [
-        *uv_run,
-        "gimmegit",
-        *helpers.no_color,
-    ]
-    result = subprocess.run(
-        command,
-        cwd=test_dir,
-        capture_output=True,
-        text=True,
-    )
-    assert result.returncode == 2
-    assert not result.stdout
-    expected_stderr = """\
-Error: No repo specified. Run 'gimmegit -h' for help.
-"""
-    assert result.stderr == expected_stderr
