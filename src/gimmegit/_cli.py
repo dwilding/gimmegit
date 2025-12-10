d9e41b62139ea751f286f7b74552d2c4d09b22ae
Dave Wilding <tech@dpw.me>
2025-12-10 15:14:31 +0800

    revert change to f_link

--- src/gimmegit/_cli.py
+++ src/gimmegit/_cli.py
@@ -313,7 +313,7 @@ def f_bold(value: str) -> str:
 
 def f_link(value: str, url: str) -> str:
     if COLOR[INFO_TO]:
-        return f"\033]8;;{url}\a{value}\033]8;;\a"
+        return f"\033]8;;{url}\a{f_blue(value)}\033]8;;\a"
     else:
         return value
 
@@ -478,7 +478,7 @@ def make_formatted_value(col: Column) -> FormattedStr:
     formatted = col.value
     plain = col.value
     if col.url:
-        formatted = f_link(f_blue(col.value), col.url)
+        formatted = f_link(col.value, col.url)
     return FormattedStr(
         formatted=formatted,
         plain=plain,
